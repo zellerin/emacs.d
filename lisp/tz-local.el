fd1b9e6c9ba9c45d39f4cf6f6ada7647ca5703e8
Zellerin <tzellerin@kpmg.com>
2017-05-24 15:31:39 +0200

    (tz-local) Dashboard and paths.

--- lisp/tz-local.el
+++ lisp/tz-local.el
@@ -6,7 +6,7 @@
 	"C:/Windows"
 	"C:/Windows/System32/Wbem"
 	"C:/Windows/System32/WindowsPowerShell/v1.0/"
-	"c:/Program Files (x86)/PuTTY/"
+	"c:/Users/tzellerin/SW/PuTTY/"
 	"C:/Users/tzellerin/SW/git/mingw64/bin/"
 	"C:/Users/tzellerin/SW/git/usr/bin/"
 	nil))
@@ -15,6 +15,24 @@
 (setenv "PATH" (concat
 		(getenv "PATH") ";C:/Users/tzellerin/SW/git/usr/bin/"))
 
+;; this is needed for tramp to work
+(setenv "PATH" (concat
+		(getenv "PATH") ";c:/Users/tzellerin/SW/PuTTY/"))
+
 (setq slime-lisp-implementations
       '((abcl ("java" "-jar"
 	       "c:/Users/tzellerin/SW/abcl-bin-1.4.0/abcl-bin-1.4.0/abcl.jar"))))
+
+(setq dashboard-items '((recents   . 5)
+			(bookmarks . 5)
+			(agenda    . 5)
+			(projects  . 5)))
+
+
+(setq dashboard-startup-banner 'official)
+
+(setq recentf-exclude '("emacs.d/elpa/"))
+(dashboard-setup-startup-hook)
+
+(setq org-directory "c:/Users/tzellerin/Documents/")
+(setq org-agenda-files (list org-directory (concat org-directory "projects")))
