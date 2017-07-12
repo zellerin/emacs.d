2560c18dd2f18bb10b8070f47f3ea5cde0bd2aec
Tomas Zellerin <zellerin@gmail.com>
2017-07-12 22:14:13 +0200

    (tz-local) Local modifications for Windows
    Corporate email, exec path, Java lisp.

--- lisp/tz-local.el
+++ lisp/tz-local.el
@@ -1,6 +1,20 @@
-(set-fontset-font  t 'symbol "-Free-Symbola-normal-normal-semicondensed-*-15-*-*-*-*-0-iso10646-1")
-(set-frame-font "-PfEd-DejaVu Sans Mono-normal-normal-normal-*-15-*-*-*-*-0-iso10646-1" t t)
+(setq user-full-name "Tomas Zellerin")
 
-(setq user-full-name "Tomas Zellerin"
-      user-mail-address "tomas@zellerin.cz")
+(setq exec-path
+      '("c:/ProgramData/Oracle/Java/javapath"
+	"C:/Windows/system32"
+	"C:/Windows"
+	"C:/Windows/System32/Wbem"
+	"C:/Windows/System32/WindowsPowerShell/v1.0/"
+	"c:/Program Files (x86)/PuTTY/"
+	"C:/Users/tzellerin/SW/git/mingw64/bin/"
+	"C:/Users/tzellerin/SW/git/usr/bin/"
+	nil))
 
+;; This is needed for magit commits to work (path to sh.exe)
+(setenv "PATH" (concat
+		(getenv "PATH") ";C:/Users/tzellerin/SW/git/usr/bin/"))
+
+(setq slime-lisp-implementations
+      '((abcl ("java" "-jar"
+	       "c:/Users/tzellerin/SW/abcl-bin-1.4.0/abcl-bin-1.4.0/abcl.jar"))))
