9099956c1d8222efeaf2cbbf9675de3d66220dbf
Tomas Zellerin <zellerin@gmail.com>
2018-01-23 19:21:08 +0100

    Windows cleanup, start documenting.

--- lisp/tz-windows.el
+++ lisp/tz-windows.el
@@ -2,36 +2,10 @@
 (setq user-full-name "Tomas Zellerin")
 
 (setq exec-path
-      '("c:/ProgramData/Oracle/Java/javapath"
-	"C:/Windows/system32"
-	"C:/Windows"
-	"C:/Windows/System32/Wbem"
-	"C:/Windows/System32/WindowsPowerShell/v1.0/"
-	"c:/Users/tzellerin/SW/PuTTY/"
-	"C:/Users/tzellerin/SW/git/mingw64/bin/"
-	"C:/Users/tzellerin/SW/git/usr/bin/"
-	"C:/Users/tzellerin/SW/emacs-w64-25.1-O2-with-modules/bin"
-	nil))
+      '("C:/ProgramData/Oracle/Java/javapath" "C:/Users/tzellerin/SW/emacs-w64-25.3-O2-with-modules/bin" "C:/Users/tzellerin/SW/git/mingw64/bin/" "C:/Users/tzellerin/SW/git/usr/bin/" "c:/Users/tzellerin/SW/graphviz/bin/" "c:/Users/tzellerin/SW/mupdf-1.11-windows/" "c:/Users/tzellerin/SW/PuTTY/" "c:/Users/tzellerin/SW/PuTTY/c:/Users/tzellerin/SW/graphviz/bin/" "c:/Users/tzellerin/SW/R-3.4.2/R/bin/x64" "C:/WINDOWS" "C:/WINDOWS/system32" "C:/WINDOWS/System32/Wbem" "C:/WINDOWS/System32/WindowsPowerShell/v1.0/" nil))
+(setenv "PATH" (mapconcat 'identity (butlast exec-path) ";"))
 
-(setq org-directory "C:/Users/tzellerin/Documents/Projects/")
-
-;; This is needed for magit commits to work (path to sh.exe)
-(setenv "PATH" (concat
-		;; I want find.exe from here to take precedence
-		"C:/Users/tzellerin/SW/git/usr/bin/;" (getenv "PATH")))
-
-;; this is needed for tramp to work
-(setenv "PATH" (concat
-		(getenv "PATH") ";c:/Users/tzellerin/SW/PuTTY/"))
-
-(setenv "PATH" (concat
-		(getenv "PATH")
-		";C:/Users/tzellerin/SW/emacs-w64-25.1-O2-with-modules/bin"))
-
-;; this is needed for git pushing
-(setenv "GIT_SSH" "plink")
-(setenv "PATH" (concat
-		(getenv "PATH") ";C:/Users/tzellerin/SW/git/mingw64/bin/"))
+(setenv "GIT_SSH" "C:/Users/tzellerin/SW/putty/plink.exe")
 
 (setq slime-lisp-implementations
       '((abcl ("java" "-jar"
@@ -66,9 +40,27 @@
 (eval-after-load "org-capture"
   '(push '("O" "Outlook" entry
 	  (file "weekly-review.org")
-	  "%(tz-outlook-capture-item)")
+	  "%(tz-outlook-capture-item \"TODO\")\n")
 	org-capture-templates))
 
 (setq experimental-logical-names
       '(("docs" . "c:/Users/tzellerin/documents/")
 	("conf" . "c:/Users/tzellerin/configs/")))
+
+(defun tz-refile-postpone ()
+  "Refile (from daily) to weekly topics. Presumably, the task was
+finished, but needs to stay in weekly file for
+tracking (especially time tracking) purposes. "
+  (interactive)
+  (org-todo "DONE")
+  (org-refile nil  nil (list "Finished" "weekly-review.org"
+			     (buffer-size (find-buffer-visiting "c:/Users/tzellerin/Documents/weekly-review.org")))))
+
+(use-package "hydra"
+  :config
+  (defhydra hydra-work (global-map "<f12>") "Work tasks"
+    ("O" tz-outlook-open-mail "Open mail")
+    ("-" tz-refile-postpone "Close and refile for tracking")))
+
+(eval-after-load "dired"
+  '(bind-key "<RET>" 'dired-w32-browser dired-mode-map))
