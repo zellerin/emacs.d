f842b9c9f8d5fefcb846052cdfc5e549f0bf99c1
Tomas Zellerin <zellerin@gmail.com>
2018-03-20 16:08:08 +0100

    Windows changes (formatting)

--- lisp/tz-windows.el
+++ lisp/tz-windows.el
@@ -2,7 +2,22 @@
 (setq user-full-name "Tomas Zellerin")
 
 (setq exec-path
-      '("C:/ProgramData/Oracle/Java/javapath" "C:/Users/tzellerin/SW/emacs-w64-25.3-O2-with-modules/bin" "C:/Users/tzellerin/SW/git/mingw64/bin/" "C:/Users/tzellerin/SW/git/usr/bin/" "c:/Users/tzellerin/SW/graphviz/bin/" "c:/Users/tzellerin/SW/mupdf-1.11-windows/" "c:/Users/tzellerin/SW/PuTTY/" "c:/Users/tzellerin/SW/graphviz/bin/" "c:/Users/tzellerin/SW/gnuplot/bin/" "c:/Users/tzellerin/SW/R-3.4.2/R/bin/x64" "C:/WINDOWS" "C:/WINDOWS/system32" "C:/WINDOWS/System32/Wbem" "C:/WINDOWS/System32/WindowsPowerShell/v1.0/" nil))
+      '("C:/ProgramData/Oracle/Java/javapath"
+	"C:/Users/tzellerin/SW/emacs-w64-25.3-O2-with-modules/bin"
+	"C:/Users/tzellerin/SW/git/mingw64/bin/"
+	"C:/Users/tzellerin/SW/git/usr/bin/"
+	"c:/Users/tzellerin/SW/graphviz/bin/"
+	"c:/Users/tzellerin/SW/mupdf-1.11-windows/"
+	"c:/Users/tzellerin/SW/PuTTY/"
+	"c:/Users/tzellerin/SW/graphviz/bin/"
+	"c:/Users/tzellerin/SW/gnuplot/bin/"
+	"c:/Users/tzellerin/SW/R-3.4.2/R/bin/x64"
+	"C:/WINDOWS"
+	"C:/WINDOWS/system32"
+	"C:/WINDOWS/System32/Wbem"
+	"C:/WINDOWS/System32/WindowsPowerShell/v1.0/"
+	nil))
+
 (setenv "PATH" (mapconcat 'identity (butlast exec-path) ";"))
 
 (setenv "GIT_SSH" "C:/Users/tzellerin/SW/putty/plink.exe")
@@ -23,10 +38,6 @@
 	    (thing-at-point 'sentence t)
 	    "]]")))
 
-(eval-after-load "org"
-  '(load-file (locate-user-emacs-file
-	      "windows-integration/tz-outlook.el")))
-
 (eval-after-load "org-capture"
   '(push '("O" "Outlook" entry
 	  (file "weekly-review.org")
@@ -39,12 +50,13 @@
 (use-package "hydra"
   :config
   (defhydra hydra-work (global-map "<f12>") "Work tasks"
-    ("o" tz-outlook-open-mail "Open mail")
-    ("f" workflow-project-setup-frame "Set up project frame")
-    ("<RET>" make-frame "Zoom to a new frame")
-    ("-" tz-refile-postpone "Close and refile for tracking")))
+    ("o"	 tz-outlook-open-mail		 "Open mail")
+    ("f"	 workflow-project-setup-frame	 "Set up project frame")
+    ("<RET>"	 make-frame			 "Zoom to a new frame")
+    ("-"	 tz-refile-postpone		 "Close and refile for tracking")))
 
 (eval-after-load "dired"
-  '(progn
-     (require 'w32-browser)
-     (bind-key "C-c <RET>" 'dired-w32-browser dired-mode-map)))
+  '(bind-key "C-c <RET>" 'dired-w32-browser dired-mode-map))
+
+(use-package "w32-browser"
+  :commands (dired-w32-browser))
