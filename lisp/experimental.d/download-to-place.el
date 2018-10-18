55f87e836a9c9057dafb1cec5fd532bd00f6775e
Tomas Zellerin <zellerin@gmail.com>
2018-10-18 15:45:00 +0200

    Create and fix org-donwload in experimental.

--- lisp/experimental.d/download-to-place.el
+++ lisp/experimental.d/download-to-place.el
@@ -1,8 +1,21 @@
-(defun tz-download-callback (file-abbrev)
-  (let ((file (org-link-expand-abbrev file-abbrev)))
-    (lambda  (status url)
-      (unless (plist-get status :error)
-	(goto-char (point-min))
-	(re-search-forward "\r?\n\r?\n")
-	(write-region (point) (point-max) file)
-	(message "Saved %s" file)))))
+;;; download-to-place.el --- Download in org mode    -*- lexical-binding: t; -*-
+(defun tz-download-callback (status file)
+  (unless (plist-get status :error)
+    (goto-char (point-min))
+    (re-search-forward "\r?\n\r?\n")
+    (let ((coding-system-for-write 'no-conversion))
+      (write-region (point) (point-max) file))
+    (message "Saved %s" file)
+    (find-file-other-frame file)))
+
+(defun org-download ()
+  (interactive)
+  (let ((context
+	 (org-element-lineage (org-element-context) '(link) t)))
+    (when context
+      (url-retrieve (cl-getf (cadr context) :raw-link)
+		    'tz-download-callback
+		    (list (concat
+			   (org-attach-dir t)
+			   (file-name-nondirectory
+			    (cl-getf (cadr context) :raw-link))))))))
