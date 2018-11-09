9b32b7daeb628ca5e57430e190b4d4d59886f98a
Tomas Zellerin <zellerin@charon.lab>
2018-11-09 16:18:30 +0100

    (experimental) nm-tz.el
    List WiFi's available.

---
+++ lisp/experimental.d/nm-tz.el
@@ -0,0 +1,21 @@
+(define-derived-mode nm-mode comint-mode
+  "Network Manager mode"
+  "Provide interface for network manager" )
+
+(defun nm-wifi ()
+  (interactive)
+  (when (get-buffer "*wifi*")
+    (kill-buffer "*wifi*"))
+  (start-process "wifi" "*wifi*" "nmcli" "dev" "wifi")
+  (switch-to-buffer "*wifi*")
+  (goto-char 1)
+  (font-lock-mode)
+  (read-only-mode)
+  (font-lock-add-keywords nil `(("WPA1" 0 'font-lock-warning-face)
+			"[0-9]+ Mbit/s" "WPA2" "802.1X"
+			("^  \\(.*\\) \\(Infra\\)"
+			 (1 '(face font-lock-variable-name-face))
+			 (2 font-lock-keyword-face))
+			("^\\*  \\(.*\\) \\(Infra\\)"
+			 (1 '(face bold))
+			 (2 font-lock-keyword-face)))))
