95b023ccf07415725d9fb259cf510397484bbb00
Tomas Zellerin <zellerin@gmail.com>
2018-11-18 13:54:30 +0100

    (experimental) download url at point

--- lisp/experimental.d/download-to-place.el
+++ lisp/experimental.d/download-to-place.el
@@ -1,8 +1,18 @@
+;;; experimental.el --- Experimental startup code    -*- lexical-binding: t; -*-
+
 (defun tz-download-callback (file-abbrev)
   (let ((file (org-link-expand-abbrev file-abbrev)))
-    (lambda  (status url)
+    (lambda  (status)
       (unless (plist-get status :error)
 	(goto-char (point-min))
 	(re-search-forward "\r?\n\r?\n")
 	(write-region (point) (point-max) file)
 	(message "Saved %s" file)))))
+
+(defun tz-download-at-point (&optional url)
+  (interactive)
+  (unless url
+    (setq url (read-string "Url: " (thing-at-point 'url))))
+  (url-retrieve
+   url
+   (tz-download-callback (concat "~/Downloads/" (file-name-nondirectory url)))))
