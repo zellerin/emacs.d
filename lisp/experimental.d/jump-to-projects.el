aa4fb48d4270efca0540c19d5cd246d500e55a3e
Tomas Zellerin <zellerin@gmail.com>
2018-11-18 14:25:56 +0100

    (experimental) Jump to projects list (f12p)

---
+++ lisp/experimental.d/jump-to-projects.el
@@ -0,0 +1,13 @@
+(defun tz-org-jump-top-projects ()
+  (interactive)
+  (if (get-buffer "Projects list")
+      (switch-to-buffer "Projects list")
+    (find-file (org-link-expand-abbrev "org:/admin.org"))
+    (outline-show-all)
+    (switch-to-buffer (make-indirect-buffer (current-buffer) "Projects list" t))
+    (goto-char (point-min))
+    (search-forward "#+RESULTS:")
+    (org-narrow-to-element)))
+
+
+(bind-key (kbd "<f12> p") 'tz-org-jump-top-projects)
