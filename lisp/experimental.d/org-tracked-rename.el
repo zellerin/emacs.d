f7cc1be74846fc45cdd190461d3decbe2b3bb705
Tomas Zellerin <zellerin@gmail.com>
2018-11-18 14:28:41 +0100

    (experimental) <f12>/ Tracked org heading change.

---
+++ lisp/experimental.d/org-tracked-rename.el
@@ -0,0 +1,14 @@
+(defun org-tracked-rename ()
+  "Rename a header and make a note about it."
+  (interactive)
+  (org-kill-line)
+  (save-excursion
+    (goto-char (org-log-beginning t))
+    (insert "   - Note taked on ")
+    (org-time-stamp '(16) t)
+    (insert " \\\\\n     Renamed from ")
+    (yank)
+    (insert "\n")))
+
+(eval-after-load 'org
+  '(bind-key "<f12> /" 'org-tracked-rename org-mode-map))
