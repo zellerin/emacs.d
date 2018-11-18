c3bde1309aaac7f3c22c968453153d308d0c82ba
Tomas Zellerin <zellerin@gmail.com>
2018-11-18 14:15:34 +0100

    (experimental) Add keybindings for download

--- lisp/experimental.d/download-to-place.el
+++ lisp/experimental.d/download-to-place.el
@@ -30,3 +30,6 @@
 			   (org-attach-dir t) "/"
 			   (file-name-nondirectory
 			    (cl-getf (cadr context) :raw-link))))))))
+
+(bind-key "<f12>D" 'org-attach-downloaded-link org-mode-map)
+(bind-key "<f12>D" 'tz-download-at-point)
