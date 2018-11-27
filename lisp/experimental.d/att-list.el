0462cfc74e957a570187c7571c55167bc3eeca5e
Tomas Zellerin <zellerin@gmail.com>
2018-11-27 13:02:25 +0100

    lisp/experimental.d/att-list.el

--- lisp/experimental.d/att-list.el
+++ lisp/experimental.d/att-list.el
@@ -1,5 +1,8 @@
 (defun tz-insert-list-of-attachments ()
-(interactive)
+  (interactive)
   (mapcar (lambda (a)
 	    (insert (format "- [[%s][%s]]\n" a (file-name-nondirectory a))))
 	  (directory-files (org-attach-dir) t "^[^.]")))
+
+(eval-after-load 'org
+  '(bind-key "<f12>A" 'tz-insert-list-of-attachments org-mode-map))
