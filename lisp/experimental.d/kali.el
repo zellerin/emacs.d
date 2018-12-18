9e0d990c484d8defc2646ee6ce7f353108020b5d
Tomas Zellerin <zellerin@charon.lab>
2018-12-18 16:05:58 +0100

    Add more Kali stuff.
    * lisp/experimental.d/kali.el (kali-rw): generic kali process for
      updates etc
    (kali-cmd): run one command, read-only
    (kali-msf): run metasploit console
    (kali-zap): run zap
    (kali-sqlmap): run sqlmap in kali
    (nmap-mode): Highlight nmap results in text
    (kali-nmap): run nmap in kali and highlight

--- lisp/experimental.d/kali.el
+++ lisp/experimental.d/kali.el
@@ -1,7 +1,50 @@
-(defun kali ()
+(defun kali-rw ()
   "Run systemd container with Kali in a buffer."
   (interactive)
   (let ((default-directory "/sudo::/"))
     (switch-to-buffer
      (make-comint "kali" "systemd-nspawn"
 		  nil  "--machine" "kali" "-D" "/opt/kali" "--bind-ro=/tmp/.X11-unix"))))
+
+(defun kali-cmd (name &rest cmd)
+  (let ((default-directory "/sudo::/"))
+    (switch-to-buffer
+     (apply 'make-comint name "systemd-nspawn"
+	    nil "--machine" "kali-msf" "--read-only" "-D" "/opt/kali" "--bind-ro=/tmp/.X11-unix" "--bind=/tmp/:/root"
+	    cmd))))
+
+(defun kali-msf ()
+  "Run systemd container with Metasploit (Kali) in a buffer."
+  (interactive)
+  (kali-cmd "kali-msf" "msfconsole" "-q"))
+
+(defun kali-zap ()
+  "Run systemd container with ZAP (Kali) in a buffer."
+  (interactive)
+  (kali-cmd "kali-zap" "zaproxy"))
+
+(defun kali-sqlmap (&rest cmds)
+  "Run systemd container with sqlmap (Kali) in a buffer."
+  (apply 'kali-cmd "kali-sqlmap" "sqlmap" cmds))
+
+(defun kali-sqlmap (&rest cmds)
+  "Run systemd container with sqlmap (Kali) in a buffer."
+  (apply 'kali-cmd "kali-sqlmap" "sqlmap" cmds))
+
+(define-minor-mode nmap-mode "Mode for nmap"
+  nil " nmap" nil
+  (font-lock-add-keywords nil `(("[0-9]+/.* open .*" 0 '(:foreground "green"))
+			("[0-9]+/.*closed.*" 0 '(:strike-through t))
+			("Nmap scan report for .*" 0 '(bold t :overline t :height 150))
+			("Nmap done.*" 0 '(:underline t :foreground "gray"))
+			("^SF[-:].*" 0 '(:foreground "gray"))
+			(,(regexp-opt '("Spawning container kali-msf on "
+					"Press ^] three times within 1s to kill container."
+					"Starting Nmap "
+					"Container kali-msf exited successfully."))
+			 0 '(:foreground "gray")))))
+
+(defun kali-nmap (&rest cmds)
+  "Run systemd container with sqlmap (Kali) in a buffer."
+  (apply 'kali-cmd "kali-nmap" "nmap" cmds)
+  (nmap-mode 1))
