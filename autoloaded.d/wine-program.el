d35b29b3e437eae0c6c05e5d3c07ff505a9ef3e9
Tomas Zellerin <zellerin@gmail.com>
2019-01-02 21:26:23 +0100

    Generalize Alpha wine32 program to wine32 from list.

---
+++ autoloaded.d/wine-program.el
@@ -0,0 +1,34 @@
+(defcustom wine-programs nil
+  "List of programs runnable by Wine"
+  :type '(repeat (list (string :tag "Name") directory (string :tag "Program")))
+  :group 'experimental)
+
+(defcustom opt-programs nil
+  "List of GUI programs to run in specific directory"
+  :type '(repeat (list (string :tag "Name") directory
+		       (string :tag "Program")
+		       (repeat string)))
+  :group 'experimental)
+
+(defun all--programs (base wine)
+  (append base
+	  (mapcar (lambda (p)
+		    `(,(car p)  ,(cadr p)
+		      "/usr/bin/wine32" ,(nthcdr 2 p)))
+			  wine)))
+
+;;;###autoload
+(defun run-gui-program ()
+  "Run a Wine program picked from a list."
+  (interactive)
+  (let* ((all-programs (all--programs opt-programs wine-programs))
+	 (program-name (completing-read "Program: "
+					all-programs
+					nil t))
+	 (program (assoc program-name all-programs))
+	 (default-directory (nth 1 program))
+	 (prog-and-args (cddr program)))
+    (apply 'make-comint program-name (car prog-and-args) nil (cadr prog-and-args))))
+
+;;;###autoload
+(bind-key "<f12> W" 'run-gui-program)
--- lisp/experimental.d/alpha-centauri.el
+++
@@ -1,6 +0,0 @@
-(defun alpha-centauri ()
-  (interactive)
-  (with-temp-buffer
-    (let ((default-directory "/opt/compressed/drive_c/Program Files/Sid Meier's Alpha Centauri"))
-      (start-process-shell-command "ALPHA" "ALPHA"
-				      "/usr/bin/wine32 ./terran.exe"))))
