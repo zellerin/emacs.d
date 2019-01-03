b76a1222dfb12415f4e07e089dd0f3d1921a0a77
Tomas Zellerin <zellerin@gmail.com>
2019-01-03 09:03:41 +0100

    Document att-list snippet.

--- lisp/experimental.d/att-list.el
+++ lisp/experimental.d/att-list.el
@@ -1,8 +1,11 @@
+;;;###autoload
 (defun tz-insert-list-of-attachments ()
+  "Insert list of attachments of an org node"
   (interactive)
   (mapcar (lambda (a)
 	    (insert (format "- [[%s][%s]]\n" a (file-name-nondirectory a))))
 	  (directory-files (org-attach-dir) t "^[^.]")))
 
+;;;###autoload
 (eval-after-load 'org
   '(bind-key "<f12>A" 'tz-insert-list-of-attachments org-mode-map))
