96cd952562d85f2d8ca475344a1487a491db90ec
Tomas Zellerin <zellerin@gmail.com>
2019-01-04 15:11:15 +0100

    Test also agenda and todo lists.

--- autoloaded.d/test.el
+++ autoloaded.d/test.el
@@ -5,13 +5,23 @@
   (message "Startup time: %.1f secs"
 	   (float-time (time-subtract after-init-time before-init-time)))
   (html-printscreen-buffer "init.html")
-  (save-excursion
-    (switch-to-buffer "*Messages*")
-    (html-printscreen-buffer "messages.html")
-    (kill-buffer))
+
   (save-excursion
     (switch-to-buffer "*Warnings*")
     (html-printscreen-buffer "warnings.html")
+    (kill-buffer))
+  (save-excursion
+    (org-agenda-list)
+    (html-printscreen-buffer "agenda.html")
+    (kill-buffer))
+  (save-excursion
+    (org-todo-list)
+    (html-printscreen-buffer "todo.html")
+    (kill-buffer))
+  (message "Time so far: %.1f secs" (float-time (time-subtract nil before-init-time)))
+  (save-excursion
+    (switch-to-buffer "*Messages*")
+    (html-printscreen-buffer "messages.html")
     (kill-buffer)))
 
 (defvar tz-emacs-printscreen-dir
