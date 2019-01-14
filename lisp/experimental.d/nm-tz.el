ebaabc1a344e188ba97a27b52c5d9b838eb8dc75
Tomas Zellerin <zellerin@charon.lab>
2019-01-14 10:22:31 +0100

    nm-wifi changes.

--- lisp/experimental.d/nm-tz.el
+++ lisp/experimental.d/nm-tz.el
@@ -1,14 +1,6 @@
-(define-derived-mode nm-mode comint-mode
+(define-derived-mode nm-mode fundamental-mode
   "Network Manager mode"
-  "Provide interface for network manager" )
-
-(defun nm-wifi ()
-  (interactive)
-  (when (get-buffer "*wifi*")
-    (kill-buffer "*wifi*"))
-  (start-process "wifi" "*wifi*" "nmcli" "dev" "wifi")
-  (switch-to-buffer "*wifi*")
-  (goto-char 1)
+  "Provide interface for network manager"
   (font-lock-mode)
   (read-only-mode)
   (font-lock-add-keywords nil `(("WPA1" 0 'font-lock-warning-face)
@@ -19,3 +11,11 @@
 			("^\\*  \\(.*\\) \\(Infra\\)"
 			 (1 '(face bold))
 			 (2 font-lock-keyword-face)))))
+
+(defun nm-wifi ()
+  (interactive)
+  (when (get-buffer "*wifi*")
+    (kill-buffer "*wifi*"))
+  (start-process "wifi" "*wifi*" "nmcli" "dev" "wifi")
+  (switch-to-buffer "*wifi*")
+  (nm-mode))
