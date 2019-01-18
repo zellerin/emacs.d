cbf999f587b3bc43354724568c4870f98c5649a3
Tomas Zellerin <zellerin@gmail.com>
2019-01-18 16:33:57 +0100

    Add simple json parser for shodan.

--- autoloaded.d/shodan.el
+++ autoloaded.d/shodan.el
@@ -10,6 +10,9 @@
 
 (require 'restclient)
 
+(defvar-local shodan-parsed-json
+  "Parsed json of the result buffer")
+
 ;###aaautoload
 (autoload 'org-babel-execute:shodan "shodan")
 
@@ -31,5 +34,57 @@
 
 (define-skeleton shodan-host "Make Shodan Host query"
   "Host: "
+  ":key " shodan-api-key ?\n
   "GET https://api.shodan.io/shodan/host/" str "?key=:key"
   '(when (y-or-n-p "History") (insert "&history=true")))
+
+(defun shodan-slurp (file)
+  (with-current-buffer (find-file file)
+    (goto-char (point-min))
+    (json-read)))
+
+(defun shodan-get-data-from-file (file path)
+  (shodan-get-data (shodan-slurp file) path))
+
+(defun shodan-get-data (base path)
+  (cond ((null path) (list base))
+	((equal :all (car path))
+	 (mapcan (lambda (el) (shodan-getdata el (cdr path)))
+		 base))
+	((equal :trim (car path))
+	 (let ((res (car (shodan-getdata base (cdr path)))))
+	   (if (stringp res) (string-trim res) res)))
+	((equal :single (car path))
+	 (car (shodan-getdata base (cdr path))))
+	((and (consp (car path))
+	      (equal '= (caar path)))
+	 (when (equalp (car (shodan-getdata base (cddr (car path))))
+		      (nth 1 (car path)))
+	   (shodan-getdata base (cdr path))))
+	((consp (car path))
+	 (list (mapcar (lambda (item)
+			 (shodan-getdata base item))
+		       (car path))))
+	((numberp (car path))
+	 (shodan-getdata (aref base (car path))
+			 (cdr path)))
+	(t (shodan-getdata
+	    (cdr (assoc (car path) base)) (cdr path)))))
+
+
+;;;; (shodan-getdata my-json '(:all ip_str))
+;;;; (shodan-getdata my-json '(:all (ip_str port (http title))))
+;;;; (message "%s" (shodan-getdata my-json '(0 ((http title) product))))
+;;;;
+;;;; (message "%s" (shodan-getdata my-json '(:all ((ssl cert subject CN)
+;;;; 					      (http title)
+;;;; 					      (product)))))
+;;;; (message "%s" (shodan-getdata my-json '(:all
+;;;; 					(= 443 port)
+;;;; 					((:single ssl cert subject CN)
+;;;; 					      (http title)
+;;;; 					      (product)))))
+;;;;
+;;;; (message "%s" (shodan-getdata my-json '(0
+;;;; 					(= 443 port) port))
+;;;; )
