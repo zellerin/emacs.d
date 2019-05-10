8be77a970912f1871677aa93a8a523859d08394f
Tomas Zellerin <zellerin@charon.lab>
2019-05-10 17:00:15 +0200

    A fix.

--- autoloaded.d/nm-tz.el
+++ autoloaded.d/nm-tz.el
@@ -1,11 +1,10 @@
+
 ;;;###autoload
 (defun nm-wifi ()
   "Provide interface for network manager"
   (interactive)
-  (when (get-buffer "*wifi*")
-    (kill-buffer "*wifi*"))
-  (start-process "wifi" "*wifi*" "nmcli" "dev" "wifi")
-  (switch-to-buffer "*wifi*")
+  (start-process "nmcli" "*nm*" "nmcli" "dev" "wifi")
+  (switch-to-buffer "*nm*")
   (nm-mode)
   (goto-char 1))
 
@@ -17,13 +16,73 @@
 
 (define-derived-mode nm-mode fundamental-mode
   "Network Manager mode"
-  (font-lock-mode)
   (read-only-mode)
-  (font-lock-add-keywords nil `(("WPA1" 0 'font-lock-warning-face)
-			"[0-9]+ Mbit/s" "WPA2" "802.1X"
-			("^  \\(.*\\) \\(Infra\\)"
-			 (1 '(face font-lock-variable-name-face))
-			 (2 font-lock-keyword-face))
-			("^\\*  \\(.*\\) \\(Infra\\)"
-			 (1 '(face bold))
-			 (2 font-lock-keyword-face)))))
+  (setq font-lock-defaults
+	`((("WPA1" 0 'font-lock-warning-face)
+	    "[0-9]+ Mbit/s" "WPA2" "802.1X"
+	    ("^  \\(.*\\) \\(Infra\\)"
+	     (1 '(face font-lock-variable-name-face))
+	     (2 font-lock-keyword-face))
+	    ("^\\*  \\(.*\\) \\(Infra\\)"
+	     (1 '(face bold))
+	     (2 font-lock-keyword-face))
+	    ("\\<connected\\>" 0 '(face bold))
+	    ("^NAME"
+	     (0 'font-lock-keyword-face)
+	     ("UUID\\|TYPE\\|DEVICE" nil nil (0 font-lock-keyword-face)))
+	    (,(regexp-opt nm-connections-list 'words) .
+	     (0 '(face bold connection t))
+	     ))))
+  (font-lock-mode))
+
+(defcustom nm-connections-list
+  '("empl" "eno1" "eno1emp")
+  "List of nmcli connections to offer up/down"
+  :group 'experimental
+  :type '(repeat string))
+
+(setq nm-mode-map
+      '(keymap
+	(?u . nm-connection-up)
+	(?d . nm-connection-down)
+	(?c . nm-connections)
+	(?x . nm-devices)
+	(?w . nm-wifi)
+	(?? . describe-mode)))
+
+(defun nm-connections ()
+  "Provide interface for network manager"
+  (interactive)
+  (start-process "nmcli" "*nm*" "nmcli" "conn")
+  (switch-to-buffer "*nm*")
+  (nm-mode))
+
+(defun nm-devices ()
+  "Provide interface for network manager"
+  (interactive)
+  (start-process "nmcli" "*nm*" "nmcli" "dev")
+  (switch-to-buffer "*nm*")
+  (nm-mode)
+  (goto-char 1))
+
+(defun nm-connection-up (&optional conn)
+  "Provide interface for network manager"
+  (interactive)
+  (setq conn
+	(or conn (if (get-text-property (point) 'connection)
+		     (word-at-point)
+		   (read-string "Connection: "))))
+  (start-process "nmcli" "*nm*" "nmcli" "conn" "up" conn)
+  (switch-to-buffer "*nm*"))
+
+(defun nm-connection-down (&optional conn)
+  "Provide interface for network manager"
+  (interactive)
+  (setq conn
+	(or conn (if (get-text-property (point) 'connection)
+		     (word-at-point)
+		   (read-string "Connection: "))))
+
+  (start-process "nmcli" "*nm*" "nmcli" "conn" "down" conn)
+  (switch-to-buffer "*nm*")
+  (nm-mode))
